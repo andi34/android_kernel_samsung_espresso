bf1b406f4c7ae1b7b74f98a048770b713a1ffbea
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-04-26 16:07:32 +0100

    ASoC: wm8994: Tune debounce rates for jack detect mode
    Use a slightly larger debounce when identifying accessory type and a
    slightly smaller one when detecting buttons in response to user feedback
    from large scale testing.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    
    Conflicts:
    	sound/soc/codecs/wm8994.c
    
    Change-Id: I770ebbe93cf9264c290d066d63fd11d33b98d770

--- sound/soc/codecs/wm8994.c
+++ sound/soc/codecs/wm8994.c
@@ -103,8 +103,8 @@ static const struct wm8958_micd_rate micdet_rates[] = {
 static const struct wm8958_micd_rate jackdet_rates[] = {
 	{ 32768,       true,  0, 1 },
 	{ 32768,       false, 0, 1 },
-	{ 44100 * 256, true,  7, 10 },
-	{ 44100 * 256, false, 7, 10 },
+	{ 44100 * 256, true,  10, 10 },
+	{ 44100 * 256, false, 7, 8 },
 };
 
 static void wm8958_micd_set_rate(struct snd_soc_codec *codec)
@@ -3595,6 +3595,8 @@ static irqreturn_t wm1811_jackdet_irq(int irq, void *data)
 
 	if (reg & WM1811_JACKDET_LVL) {
 		dev_dbg(codec->dev, "Jack detected\n");
+
+		wm8958_micd_set_rate(codec);
 
 		snd_soc_jack_report(wm8994->micdet[0].jack,
 				    SND_JACK_MECHANICAL, SND_JACK_MECHANICAL);
