fbfe854dc119a082c95cd3eb6a4792a3184ad3ce
Seungwon Jeon <tgih.jun@samsung.com>
2012-06-18 14:42:38 +0900

    mmc: core: Add packed command feature of eMMC4.5
    This patch adds packed command feature of eMMC4.5.
    The maximum number for packing read(or write) is offered
    and exception event relevant to packed command which is
    used for error handling is enabled. If host wants to use
    this feature, MMC_CAP2_PACKED_CMD should be set.
    
    Change-Id: I747efd66c69b6ba032736812ec827a98970c5e1b
    Signed-off-by: Seungwon Jeon <tgih.jun@samsung.com>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -233,6 +233,10 @@ struct mmc_host {
 #define MMC_CAP_MAX_CURRENT_800	(1 << 29)	/* Host max current limit is 800mA */
 #define MMC_CAP_CMD23		(1 << 30)	/* CMD23 supported. */
 #define MMC_CAP_HW_RESET	(1 << 31)	/* Hardware reset */
+#define MMC_CAP2_PACKED_RD	(1 << 3)	/* Allow packed read */
+#define MMC_CAP2_PACKED_WR	(1 << 4)	/* Allow packed write */
+#define MMC_CAP2_PACKED_CMD	(MMC_CAP2_PACKED_RD | \
+				 MMC_CAP2_PACKED_WR) /* Allow packed commands */
 
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
 
