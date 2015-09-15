745a81bf111402941956a328ec141e0b681dbcc4
Andreas Blaesius <skate4life@gmx.de>
2015-09-15 22:05:58 +0200

    move samsungs bcmdhd source to bcm433x

--- drivers/net/wireless/bcm433x/dhd_linux.c
+++ drivers/net/wireless/bcm433x/dhd_linux.c
@@ -519,7 +519,7 @@ module_param(dhd_pktgen_len, uint, 0);
 /* Version string to report */
 #ifdef DHD_DEBUG
 #ifndef SRCBASE
-#define SRCBASE        "drivers/net/wireless/bcmdhd"
+#define SRCBASE        "drivers/net/wireless/bcm433x"
 #endif
 #define DHD_COMPILED "\nCompiled in " SRCBASE
 #else
