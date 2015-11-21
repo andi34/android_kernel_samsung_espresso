abdab61db0fdb5ba51a57a72d7c77be63e47da29
Andreas Blaesius <skate4life@gmx.de>
2015-11-21 21:12:23 +0100

    Remove leftover from "mmc: core: mmc sanitize feature support for v4.5"
    http://permalink.gmane.org/gmane.linux.kernel.mmc/9504
    
    Change-Id: Ifd7b50016b5cf769f3f20f0f3a96a94aaabb0825

--- include/linux/mmc/mmc.h
+++ include/linux/mmc/mmc.h
@@ -283,7 +283,6 @@ struct _mmc_csd {
 #define EXT_CSD_PARTITION_SUPPORT	160	/* RO */
 #define EXT_CSD_HPI_MGMT		161	/* R/W */
 #define EXT_CSD_RST_N_FUNCTION		162	/* R/W */
-#define EXT_CSD_SANITIZE_START		165     /* W */
 #define EXT_CSD_WR_REL_PARAM		166	/* RO */
 #define EXT_CSD_ERASE_GROUP_DEF		175	/* R/W */
 #define EXT_CSD_BOOT_CONFIG_PROT	178	/* R/W */
@@ -347,7 +346,6 @@ struct _mmc_csd {
 #define EXT_CSD_SEC_ER_EN	BIT(0)
 #define EXT_CSD_SEC_BD_BLK_EN	BIT(2)
 #define EXT_CSD_SEC_GB_CL_EN	BIT(4)
-#define EXT_CSD_SEC_SANITIZE	BIT(6)  /* v4.5 only */
 
 #define EXT_CSD_RST_N_EN_MASK	0x3
 #define EXT_CSD_RST_N_ENABLED	1	/* RST_n is enabled on card */
