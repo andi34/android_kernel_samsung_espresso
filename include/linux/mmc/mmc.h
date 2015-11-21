421bee054f95b63dc7e4551b90273206a0e4721b
Seungwon Jeon <tgih.jun@samsung.com>
2015-11-21 20:35:01 +0100

    (PARTIALLY) mmc: core: Add default timeout value for CMD6
    EXT_CSD[248] includes the default maximum timeout for CMD6.
    This field is added at eMMC4.5 Spec.
    
    Initial commit: https://lkml.org/lkml/2011/8/26/40
    
    Change-Id: I5dfc7ac9ceb067eea375d5a93a4573bd44a62a02
    Signed-off-by: Seungwon Jeon <tgih.jun@samsung.com>

--- include/linux/mmc/mmc.h
+++ include/linux/mmc/mmc.h
@@ -303,6 +303,7 @@ struct _mmc_csd {
 #define EXT_CSD_SEC_FEATURE_SUPPORT	231	/* RO */
 #define EXT_CSD_TRIM_MULT		232	/* RO */
 #define EXT_CSD_POWER_OFF_LONG_TIME	247	/* RO */
+#define EXT_CSD_GENERIC_CMD6_TIME	248	/* RO */
 #define EXT_CSD_CACHE_SIZE		249	/* RO, 4 bytes */
 #define EXT_CSD_MAX_PACKED_WRITES	500	/* RO */
 #define EXT_CSD_MAX_PACKED_READS	501	/* RO */
