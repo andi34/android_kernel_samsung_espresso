779239439bf75845f67bce14192d2376cace778d
Andreas Blaesius <skate4life@gmx.de>
2015-11-23 21:14:19 +0100

    Revert "(PARTIALLY) mmc: core: Add power off notify feature(eMMC 4.5)"
    Change-Id: I1cd7fac5daf5f687fafd12f8af7b5f621febb904

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -235,7 +235,6 @@ struct mmc_host {
 #define MMC_CAP_HW_RESET	(1 << 31)	/* Hardware reset */
 
 	unsigned int		caps2;		/* More host capabilities */
-#define MMC_CAP2_POWEROFF_NOTIFY (1 << 2)	/* Notify poweroff supported */
 #define MMC_CAP2_PACKED_RD	(1 << 3)	/* Allow packed read */
 #define MMC_CAP2_PACKED_WR	(1 << 4)	/* Allow packed write */
 #define MMC_CAP2_PACKED_CMD	(MMC_CAP2_PACKED_RD | \
