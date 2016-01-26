1044ab80e05e7cc0688b7d205171c38a2489eedf
Andreas Blaesius <skate4life@gmx.de>
2016-01-26 23:30:39 +0100

    Add cherry-picker files

---
+++ pick-f2fs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git fetch https://github.com/andi34/android_kernel_samsung_espresso.git p-android-omap-3.0.101-dev-f2fs
+echo "Cherry-picking commits security..."
+for COMMIT in `cat f2fs.txt | grep -v ^# | grep -v ^$`; do
+    git cherry-pick $COMMIT
+done
