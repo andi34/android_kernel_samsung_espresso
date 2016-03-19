0a40590b187c3424192e2072ca8b0cfa2face75d
Ziyan <jaraidaniel@gmail.com>
2016-03-19 18:33:01 +0100

    Unify espresso/espresso10: modems
    Change-Id: I0c2e10d186594c1a5d28248e78faaf4acd701d13

--- arch/arm/mach-omap2/board-espresso-modems.c
+++ arch/arm/mach-omap2/board-espresso-modems.c
@@ -209,7 +209,9 @@ static void __init umts_modem_cfg_gpio(void)
 	umts_modem_data.gpio_pda_active = modem_gpios[GPIO_PDA_ACTIVE].gpio;
 	umts_modem_data.gpio_phone_active = modem_gpios[GPIO_PHONE_ACTIVE].gpio;
 	umts_modem_data.gpio_cp_dump_int = modem_gpios[GPIO_CP_DUMP_INT].gpio;
-	umts_modem_data.gpio_sim_detect = modem_gpios[GPIO_SIM_DETECT].gpio;
+	if (!board_is_espresso10()) {
+		umts_modem_data.gpio_sim_detect = modem_gpios[GPIO_SIM_DETECT].gpio;
+	}
 
 	pr_debug("umts_modem_cfg_gpio done\n");
 }
