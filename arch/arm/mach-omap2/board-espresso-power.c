e79409febae7bbfa591c218e72b8ae5139c32143
Ziyan <jaraidaniel@gmail.com>
2016-03-19 23:52:04 +0100

    Unify espresso/espresso10: power
    Change-Id: Ic39ff6af9275adca12db67a4fc531b63a9691d6b

--- arch/arm/mach-omap2/board-espresso-power.c
+++ arch/arm/mach-omap2/board-espresso-power.c
@@ -55,6 +55,11 @@
 #define LOW_BLOCK_TEMP          (-50)
 #define LOW_RECOVER_TEMP        0
 
+#define BB_HIGH_BLOCK_TEMP         480
+#define BB_HIGH_RECOVER_TEMP       440
+#define BB_LOW_BLOCK_TEMP          (-40)
+#define BB_LOW_RECOVER_TEMP        0
+
 struct max17042_fuelgauge_callbacks *fuelgauge_callback;
 struct smb_charger_callbacks *espresso_charger_callbacks;
 struct battery_manager_callbacks *batman_callback;
@@ -209,6 +214,14 @@ static const __initdata struct i2c_board_info smb136_i2c[] = {
 		.platform_data = &smb_pdata,
 	},
 };
+
+static const __initdata struct i2c_board_info smb347_i2c[] = {
+	{
+		I2C_BOARD_INFO("smb347-charger", 0x0C >> 1),
+		.platform_data = &smb_pdata,
+	},
+};
+
 
 static void max17042_fuelgauge_register_callbacks(
 		struct max17042_fuelgauge_callbacks *ptr)
@@ -378,11 +391,16 @@ void __init omap4_espresso_charger_init(void)
 
 	charger_gpio_init();
 	espresso_gpio_i2c_init();
+	if (board_is_espresso10() && board_is_bestbuy_variant() && sec_bootmode == 5) {
+		battery_manager_pdata.high_block_temp = BB_HIGH_BLOCK_TEMP;
+		battery_manager_pdata.high_recover_temp = BB_HIGH_RECOVER_TEMP;
+		battery_manager_pdata.low_block_temp = BB_LOW_BLOCK_TEMP;
+		battery_manager_pdata.low_recover_temp = BB_LOW_RECOVER_TEMP;
+	}
 
 	battery_manager_pdata.bootmode = sec_bootmode;
-	smb_pdata.hw_revision = system_rev;
-	pr_info("%s: HW REVISION : %x\n",
-		__func__, smb_pdata.hw_revision);
+	if (!board_is_espresso10())
+		smb_pdata.hw_revision = system_rev;
 
 	battery_manager_pdata.ta_gpio =
 			omap_muxtbl_get_gpio_by_name("TA_nCONNECTED");
@@ -398,7 +416,17 @@ void __init omap4_espresso_charger_init(void)
 	if (ret < 0)
 		pr_err("%s: gpio_i2c7 device register fail\n", __func__);
 
-	i2c_register_board_info(5, smb136_i2c, ARRAY_SIZE(smb136_i2c));
+	if (board_is_espresso10()) {
+		i2c_register_board_info(5, smb347_i2c, ARRAY_SIZE(smb347_i2c));
+		max17042_pdata.sdi_capacity = 0x3730;
+		max17042_pdata.sdi_vfcapacity = 0x4996;
+		max17042_pdata.byd_capacity = 0x36B0;
+		max17042_pdata.byd_vfcapacity = 0x48EA;
+		max17042_pdata.sdi_low_bat_comp_start_vol = 3600;
+		max17042_pdata.byd_low_bat_comp_start_vol = 3650;
+	} else {
+		i2c_register_board_info(5, smb136_i2c, ARRAY_SIZE(smb136_i2c));
+	}
 	i2c_register_board_info(7, max17042_i2c, ARRAY_SIZE(max17042_i2c));
 
 	ret = platform_device_register(&battery_manager_device);
